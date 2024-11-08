d4e5c76668c72a47854e1f9509a7f1bb4456863e
nigga <jusyur@seiko.lol>
2024-11-08 02:29:24 -0600

    added quote

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,7 +6,10 @@ import random
 import discord
 import asyncio
 from datetime import datetime, timedelta
-
+from typing import Optional
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+from io import BytesIO
+import textwrap
 from greed.tool import aliases
 
 # from greed.tool import aliases
@@ -75,6 +78,91 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.blacktea = BlackTea(self.bot)
+
+    async def get_caption(self, ctx: Context, message: Optional[Message] = None):
+        if message is None:
+            msg = ctx.message.reference
+            if msg is None:
+                return await ctx.warn(f"no **message** or **reference** provided")
+            id = msg.message_id
+            message = await ctx.fetch_message(id)
+
+        image = BytesIO(await message.author.display_avatar.read())
+        image.seek(0)
+        if message.content.replace("\n", "").isascii():
+            para = textwrap.wrap(message.clean_content, width=26)
+        else:
+            para = textwrap.wrap(message.clean_content, width=13)
+
+        async def do_caption(para, image, message):
+            icon = Image.open(image)
+            haikei = Image.open("quote/grad.jpeg")
+            black = Image.open("quote/black.jpeg")
+            w, h = (680, 370)
+            haikei = haikei.resize((w, h))
+            black = black.resize((w, h))
+            icon = icon.resize((h, h))
+
+            new = Image.new(mode="L", size=(w, h))
+            icon = icon.convert("L")
+            black = black.convert("L")
+            icon = icon.crop((40, 0, 680, 370))
+            new.paste(icon)
+
+            sa = Image.composite(new, black, haikei.convert("L"))
+            draw = ImageDraw.Draw(sa)
+            fnt = ImageFont.truetype("quote/Arial.ttf", 28)
+
+            _, _, w2, h2 = draw.textbbox((0, 0), "a", font=fnt)
+            i = (int(len(para) / 2) * w2) + len(para) * 5
+            current_h, pad = 120 - i, 0
+
+            for line in para:
+                if message.content.replace("\n", "").isascii():
+                    _, _, w3, h3 = draw.textbbox(
+                        (0, 0), line.ljust(int(len(line) / 2 + 11), " "), font=fnt
+                    )
+                    draw.text(
+                        (11 * (w - w3) / 13 + 10, current_h + h2),
+                        line.ljust(int(len(line) / 2 + 11), " "),
+                        font=fnt,
+                        fill="#FFF",
+                    )
+                else:
+                    _, _, w3, h3 = draw.textbbox(
+                        (0, 0), line.ljust(int(len(line) / 2 + 5), "　"), font=fnt
+                    )
+                    draw.text(
+                        (11 * (w - w3) / 13 + 10, current_h + h2),
+                        line.ljust(int(len(line) / 2 + 5), "　"),
+                        font=fnt,
+                        fill="#FFF",
+                    )
+
+                current_h += h3 + pad
+
+            dr = ImageDraw.Draw(sa)
+            font = ImageFont.truetype("quote/Arial.ttf", 15)
+            _, _, authorw, _ = dr.textbbox(
+                (0, 0), f"-{str(message.author)}", font=font
+            )
+
+            output = BytesIO()
+            dr.text(
+                (480 - int(authorw / 2), current_h + h2 + 10),
+                f"-{str(message.author)}",
+                font=font,
+                fill="#FFF",
+            )
+            sa.save(output, format="JPEG")
+            output.seek(0)
+            return output
+
+        loop = asyncio.get_event_loop()
+        output = await loop.create_task(do_caption(para, image, message))
+        file = discord.File(fp=output, filename="quote.png")
+        return await ctx.send(file=file)
+
 
     @commands.command(name="uwuify", brief="uwuify a message", aliases=["uwu"])
     async def uwuify(self, ctx: Context, *, message: str):
@@ -357,12 +445,16 @@ class Fun(commands.Cog):
 
         await ctx.send(f"> You have set your flavor to {flavor}")
 
-    @vape.command(
-        name="individual",
-        aliases=["switch", "normal"],
-        brief="switches to user vape"
-    )
-    async def vape_normal(self, ctx): ...
+    # @vape.command(
+    #     name="individual",
+    #     aliases=["switch", "normal"],
+    #     brief="switches to user vape"
+    # )
+    # async def vape_normal(self, ctx): ...
+
+    @command(name="caption", aliases=["quote"], brief="hello world")
+    async def caption(self, ctx: Context, message: Optional[Message] = None) -> Message:
+        return await self.get_caption(ctx, message)
 
 
 async def setup(bot):
