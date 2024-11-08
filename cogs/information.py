e9f9c5166f3c0c3a445b4307b4aebdecdf374e4b
n830008 <nrollins189@gmail.com>
2024-11-08 02:02:00 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -875,7 +875,7 @@ class Information(commands.Cog):
         if is_boosting(user) is True:
             badges.extend((emojis.get("nitro"), emojis.get("server_boost")))
 
-        osguild = bot.get_guild()
+        osguild = bot.get_guild(1301617147964821524)
         devices = (
             ", ".join(
                 tuple(
