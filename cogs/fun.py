9c05891d2f169e2986478366eead1bf1e748039f
n830008 <nrollins189@gmail.com>
2024-11-08 09:50:45 +0000

    auto commit

--- cogs/fun.py
+++ cogs/fun.py
@@ -358,7 +358,7 @@ class Fun(commands.Cog):
           brief="Hit the vape",
           invoke_without_command=True
      )
-     @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.cooldown(1, 10, commands.BucketType.user)
      async def vape(self, ctx):
           has_vape = await self.bot.db.fetchrow(
                "SELECT holder FROM vape WHERE guild_id = $1", ctx.guild.id
