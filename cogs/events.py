ee0990ab99f84839ab3b8cf2151d440a55aedf49
n830008 <nrollins189@gmail.com>
2024-11-08 01:13:27 +0000

    auto commit

--- cogs/events.py
+++ cogs/events.py
@@ -192,6 +192,7 @@ class Events(commands.Cog):
         return choice.display_avatar.url
     @commands.Cog.listener("on_message")
     async def imageonly(self, message: discord.Message):
+        
     @commands.Cog.listener("on_audit_log_entry_create")
     async def moderation_logs(self, entry: discord.AuditLogEntry):
         return await self.bot.modlogs.do_log(entry)
