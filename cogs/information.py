cb224cc4c558b84b618df03907a6d873bda1cca4
n830008 <nrollins189@gmail.com>
2024-11-12 13:31:53 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -934,7 +934,7 @@ class Information(commands.Cog):
             status = "N/A"
         embed = discord.Embed(
             color=self.bot.color,
-            title
+            title=
             description=f"   \n{user.name} {badges} \n{status_emoji}{status}",
         )
 
