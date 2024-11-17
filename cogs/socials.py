456b93a56d3d0095baf75fcdf1a334ec4fc14a1b
n830008 <nrollins189@gmail.com>
2024-11-17 01:37:38 +0000

    auto commit

---
+++ cogs/socials.py
@@ -0,0 +1,26 @@
+from tool.pinterest import Pinterest  # type: ignore
+from discord.ext import commands
+import discord
+import io
+from discord.ext.commands import Context
+from aiohttp import ClientSession as Session
+from tool.pinpostmodels import Model  # type: ignore
+from tuuid import tuuid
+import requests
+import json
+
+
+
+
+class Socials(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.pinterest = Pinterest()
+
+
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(Socials(bot))
