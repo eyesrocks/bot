483429375da770dd191c4d87c0956eadbfb44fde
n830008 <nrollins189@gmail.com>
2024-11-19 20:15:34 +0000

    auto commit

--- cogs/vanity.py
+++ cogs/vanity.py
@@ -1,12 +1,12 @@
-# from discord.ext import commands, tasks
-# import discord
-# import asyncio
-# from collections import defaultdict
-# from typing import Optional
-# from discord.ext.commands import Context
-# from tool.important.subclasses.command import Role, TextChannel
+from discord.ext import commands, tasks
+import discord
+import asyncio
+from collections import defaultdict
+from typing import Optional
+from discord.ext.commands import Context
+from tool.important.subclasses.command import TextChannel
 # from loguru import logger
-# from tool.important.subclasses.parser import Script
+from tool.important.subclasses.parser import Script, EmbedConverter
 
 
 # class EmbedConverter(commands.Converter):
@@ -19,9 +19,9 @@
 #         return code
 
 
-# class Vanity(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
+class Vanity(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 #         self.local_addr = ("23.160.168.195", 0)
 #         self.helper = discord.ExpiringDictionary()
 #         self.locks = defaultdict(asyncio.Lock)
@@ -114,15 +114,81 @@
 #                     *[self.check_status(member, role_id) for member in guild.members]
 #                 )
 
-#     @commands.group(
-#         name="vanity",
+    @commands.group(
+        name="vanity",
 #         brief="Reward users with a role for repping the vanity",
-#         example=",vanity",
-#         invoke_without_command=True,
-#     )
-#     @commands.has_permissions(manage_roles=True)
-#     async def vanity(self, ctx: Context):
-#         await ctx.send_help(ctx.command.qualified_name)
+        example=",vanity",
+        invoke_without_command=True,
+    )
+    @commands.has_permissions(manage_roles=True)
+    async def vanity(self, ctx: Context):
+        await ctx.send_help(ctx.command.qualified_name)
+
+    @vanity.command(
+        name="set",
+        brief="set the channel for checking vanities", 
+        example=",vanity set #vanity-updates"
+    )
+    async def vanity_set(self, ctx, channel: TextChannel):
+        await self.bot.db.execute(
+            """INSERT INTO vanity (guild_id, channel_id) VALUES($1, $2) ON CONFLICT (guild_id) DO UPDATE SET channel_id = excluded.channel_id""",
+            ctx.guild.id,
+            channel.id,
+        )
+        return await ctx.success(f"**Vanity channel** set to {channel.mention}")
+    
+    @vanity.command(
+        name="message",
+        brief="Set the message",
+        example=",vanity message {embed}{description: thanks for repping {user.mention}}"
+    )
+    async def vanity_message(self, ctx: Context, *, message: EmbedConverter):
+        await self.bot.db.execute(
+            """UPDATE vanity SET message = $2 WHERE guild_id = $1""",
+            ctx.guild.id,
+            message,
+        )
+        await ctx.success("Vanity message has been set")
+    
+    @vanity.command(
+        name="view",
+        aliases=["config", "cfg", "settings"],
+        brief="View your vanity status settings",
+    )
+    @commands.has_permissions(manage_roles=True)
+    async def vanity_view(self, ctx: Context):
+        data = await self.bot.db.fetchrow(
+            """SELECT channel_id, message FROM vanity WHERE guild_id = $1""",
+            ctx.guild.id,
+        )
+        if not data:
+            return await ctx.fail("Vanity sniping is not set up")
+        desc = ""
+        if channel := ctx.guild.get_channel(data["channel_id"]):
+            desc += f"> **Channel:** {channel.mention}\n"
+        if message := data["message"]:
+            desc += f"> **Message:** `{message}`\n"
+        embed = discord.Embed(
+            title="Vanity Status Config", color=self.bot.color, description=desc
+        )
+        await ctx.send(embed=embed)
+
+    @commands.Cog.listener("on_guild_update")
+    async def vanity_check(self, before: discord.Guild, after: discord.Guild):
+        if before.vanity_url_code == after.vanity_url_code:
+            return
+        
+        check = await self.bot.db.fetchrow(
+            """SELECT channel_id, message FROM vanity WHERE guild_id = $1""",
+            after.id,
+        )
+        if check:
+            channel = self.bot.get_channel(check["channel_id"])
+            message = check["message"]
+            if message is None:
+                return await channel.send(f"**{after.name}** dropped their vanity {before.vanity_url_code}")
+            return await self.bot.send_embed(channel, message, guild=after)
+
 
 #     @vanity.command(
 #         name="role", brief="Set the reward role", example=",vanity role @pic"
@@ -218,5 +284,5 @@
 #         await ctx.success("Vanity status configuration reset")
 
 
-# async def setup(bot):
-#     await bot.add_cog(Vanity(bot))
+async def setup(bot):
+    await bot.add_cog(Vanity(bot))
