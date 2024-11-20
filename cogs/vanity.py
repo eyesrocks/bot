64b080ff459e4d27ce58a0b6233ab91ba2731cb7
n830008 <nrollins189@gmail.com>
2024-11-20 20:17:59 +0000

    auto commit

--- cogs/vanity.py
+++ cogs/vanity.py
@@ -114,7 +114,7 @@ class Vanity(commands.Cog):
 
     # Create the embed
         embed = discord.Embed(
-            title="Vanity URL Changed",  # Title of the embed
+            title="new vanity",  # Title of the embed
             description=message,  # The main body of the embed (the message)
             color=self.bot.color,  # You can change the color of the embed
         )
