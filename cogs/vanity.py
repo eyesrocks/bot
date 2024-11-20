1a7cec6962ae23ca81cf2ebbf863de0a50d9ae8c
n830008 <nrollins189@gmail.com>
2024-11-20 20:15:28 +0000

    auto commit

--- cogs/vanity.py
+++ cogs/vanity.py
@@ -121,7 +121,7 @@ class Vanity(commands.Cog):
 
        try:
         # Send the embed to the channel
-        await channel.send(embed=embed)
+           await channel.send(embed=embed)
     except Exception as e:
         logger.error(f"Failed to send vanity notification: {e}")
 
