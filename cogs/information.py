c6761571827e40ba5ffbc3227ff4ab01f63620a1
n830008 <nrollins189@gmail.com>
2024-11-21 17:35:27 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -1698,7 +1698,7 @@ class Information(commands.Cog):
 
         # Create buttons for Monthly and Lifetime
         monthly_button = Button(label="Monthly - $3.50", style=discord.ButtonStyle.green, url="https://buy.stripe.com/aEUdUJc8w3uf6qsfZ7")
-        lifetime_button = Button(label="Lifetime - $6", style=discord.ButtonStyle.blurple, url="https://buy.stripe.com/4gw3g52xW4yj2acdR0e")
+        lifetime_button = Button(label="Lifetime - $6", style=discord.ButtonStyle.blurple, url="https://buy.stripe.com/dR6dUJb4s5Cn4ik3co")
 
         # Define a view with the buttons
         view = View()
