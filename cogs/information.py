8da20bb036e053ccaa07155308864e8d97de6395
n830008 <nrollins189@gmail.com>
2024-11-24 15:51:11 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -1307,7 +1307,7 @@ class Information(commands.Cog):
             await ctx.warning(embed=embed)
         except Exception as e:
             embed = discord.Embed(
-                description=f"> No messages found in this channel",
+                description=f"> No messages found in this channel.",
                 color=0x2C2D31,
             )
             await ctx.warning(embed=embed)
