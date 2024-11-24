c8f99dad63618f244241f0bd15b16de4a22f55aa
n830008 <nrollins189@gmail.com>
2024-11-24 16:53:55 +0000

    auto commit

--- cogs/fun.py
+++ cogs/fun.py
@@ -732,7 +732,33 @@ class Fun(commands.Cog):
         await ctx.reply(embed=embed, mention_author=False)
 
 
-
+    @commands.hybrid_command(help="ask the :8ball: anything", aliases=["8ball"], description="fun", usage="<member>")
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    @blacklist()
+    async def eightball(self, ctx, *, question):
+        responses  = ["It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes - definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful.",
+                    "Maybe."]
+        embed=discord.Embed(color=0x2B2D31, description= f" :8ball: {random.choice(responses)}")
+        await ctx.reply(embed=embed, mention_author=False)
 
 
 async def setup(bot):
