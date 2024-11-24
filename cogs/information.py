de7496f0160cac006704144fcdeeedcb5b5503e2
n830008 <nrollins189@gmail.com>
2024-11-24 16:09:34 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -1295,7 +1295,7 @@ class Information(commands.Cog):
             async for message in channel.history(limit=1, oldest_first=True):
                 if message:
                     link = message.jump_url
-                    embed = discord.Embed
+                    embed = discord.Embed()
                     )
                     await ctx.success("> [First message]({link}) in {channel.mention}\n\n**Message content**:\n{message.content}", embed=embed)
                     return
