ee30137a83ef811ed005f5041f3cf46c460a5aaa
n830008 <nrollins189@gmail.com>
2024-11-24 15:58:39 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -1298,7 +1298,7 @@ class Information(commands.Cog):
                     description=f"> [First message]({link}) in {channel.mention}",
                     color=0x2C2D31,
                 )
-                await ctx.send(embed=embed)
+                await ctx.success(embed=embed)
                 return
 
             embed = discord.Embed(
