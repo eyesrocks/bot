927033a2906540d41daf4bc8004d59b9140b0160
n830008 <nrollins189@gmail.com>
2024-11-24 17:11:16 +0000

    auto commit

--- cogs/information.py
+++ cogs/information.py
@@ -1817,7 +1817,7 @@ class Information(commands.Cog):
     )
     async def support(self, ctx):
         """Get the support server invite link."""
-        await ctx.success("[greed support](https://discord.gg/pomice)")
+        await ctx.success("[greed ticket support](https://discord.gg/pomice)")
 
 
 
