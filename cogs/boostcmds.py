e8b0746d21157bd6b7c48f81d645e27b00136e29
root <tourow>
2024-12-05 05:56:59 +0000

    Auto commit

--- cogs/boostcmds.py
+++ cogs/boostcmds.py
@@ -5,7 +5,7 @@ import tweepy
 
 
 API_KEY = "0tu5fzJfWqv0wf3PZKYw4Fb7r"
-API_SECRET_KEY = "your_api_secret_key"
+API_SECRET_KEY = "frYCbYrvZqbX60YWz1mB6XxUYasM2M6zPLlOJsZlumHdOwB1Rp"
 ACCESS_TOKEN = "your_access_token"
 ACCESS_TOKEN_SECRET = "your_access_token_secret"
 
