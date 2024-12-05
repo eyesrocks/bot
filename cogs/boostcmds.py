72be2b55862bd82c7e5953002982f8a08a5c9d39
root <tourow>
2024-12-05 05:57:12 +0000

    Auto commit

--- cogs/boostcmds.py
+++ cogs/boostcmds.py
@@ -6,7 +6,7 @@ import tweepy
 
 API_KEY = "0tu5fzJfWqv0wf3PZKYw4Fb7r"
 API_SECRET_KEY = "frYCbYrvZqbX60YWz1mB6XxUYasM2M6zPLlOJsZlumHdOwB1Rp"
-ACCESS_TOKEN = "your_access_token"
+ACCESS_TOKEN = "1864548564568854528-IDHDW5mmQwLgghIzxShW60JVuWsgrt"
 ACCESS_TOKEN_SECRET = "your_access_token_secret"
 
 # Set up the Twitter API client
