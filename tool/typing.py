a65f733d52f977eadb6534f64fdb6a0717bd2c03
! nxyy <nxyy@nxyy.store>
2025-04-13 16:15:38 -0400

    d

--- tool/typing.py
+++ tool/typing.py
@@ -1,16 +1,23 @@
-import asyncio
-import discord
+import asyncio, discord
 from discord.ext import commands
 from typing import Optional
+from asyncio import(
+    Future,
+    AbstractEventLoop,
+    Task,
+    CancelledError,
+    TracebackType,
+)
 
 
-def handle_typing_task_completion(fut: asyncio.Future) -> None:
+
+def handle_typing_task_completion(fut: Future) -> None:
     """
     Callback for handling task completion or exceptions in typing tasks.
     """
     try:
         fut.exception()
-    except (asyncio.CancelledError, Exception):
+    except (CancelledError, Exception):
         pass
 
 
@@ -20,9 +27,9 @@ class Typing:
     """
 
     def __init__(self, ctx: commands.Context) -> None:
-        self.loop: asyncio.AbstractEventLoop = ctx.bot.loop
+        self.loop: AbstractEventLoop = ctx.bot.loop
         self.channel: discord.TextChannel = ctx.channel
-        self.task: Optional[asyncio.Task[None]] = None
+        self.task: Optional[Task[None]] = None
 
     async def do_typing(self) -> None:
         """
@@ -44,7 +51,7 @@ class Typing:
         self,
         exc_type: Optional[type[BaseException]],
         exc: Optional[BaseException],
-        tb: Optional[asyncio.TracebackType],
+        tb: Optional[TracebackType],
     ) -> None:
         """
         Cancels the typing task when exiting the context.
@@ -53,7 +60,7 @@ class Typing:
             self.task.cancel()
             try:
                 await self.task
-            except asyncio.CancelledError:
+            except CancelledError:
                 pass
 
     async def trigger_once(self) -> None:
